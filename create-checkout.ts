d1dffa2e66f6b2e8caaa00791f0ab604e7a77c48
maxflow2025 <longdang@maxflowtc.com>
2025-12-26 10:53:06 -0700

    Add files via upload

---
+++ create-checkout.ts
@@ -0,0 +1,10 @@
+step 5 
+add on:
+
+do you have Permit? yes / No . if yes make it upload require if no by pass
+
+Do you have TCP- Traffic Control Plan? Yes / No . if yes upload require if no we will provide. regular $250 per page in 10 business day.
+
+rush $1250 per page electric pdf file email to client within 8 hours
+
+tell it dont change or remove step 5 ever after that
